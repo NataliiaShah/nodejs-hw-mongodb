1f680b910d1d480940e7f14aed70af258e3f6334
NataliiaShah <gavrilovskaboroluk@gmail.com>
2025-02-28 23:38:15 +0200

    correction

--- src/db/initMongoConnection.js
+++ src/db/initMongoConnection.js
@@ -2,8 +2,8 @@ import dotenv from "dotenv";
 import mongoose from "mongoose";
 
 dotenv.config();
-const DB_URI = process.env.MONGO_URI;
+const DB_URL = process.env.MONGO_URL;
 
 export function initMongoConnection() {
-    return mongoose.connect(DB_URI);
+    return mongoose.connect(DB_URL);
 }
