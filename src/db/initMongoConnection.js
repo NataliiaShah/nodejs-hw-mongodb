c6d26c9c104f0ddc6b8bd53a501dee4407955911
NataliiaShah <gavrilovskaboroluk@gmail.com>
2025-03-01 22:09:41 +0200

    сщккусешщт

--- src/db/initMongoConnection.js
+++ src/db/initMongoConnection.js
@@ -2,7 +2,7 @@ import dotenv from "dotenv";
 import mongoose from "mongoose";
 
 dotenv.config();
-const DB_URL = process.env.MONGO_URL;
+const DB_URL = process.env.MONGODB_URL;
 
 export function initMongoConnection() {
     return mongoose.connect(DB_URL);
