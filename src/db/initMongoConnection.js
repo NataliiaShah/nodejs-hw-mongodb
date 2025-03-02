6265602ef46b67e1840f80b73ee68d0a209de85f
NataliiaShah <gavrilovskaboroluk@gmail.com>
2025-03-02 18:56:16 +0200

    correction

--- src/db/initMongoConnection.js
+++ src/db/initMongoConnection.js
@@ -10,6 +10,7 @@ export const initMongoConnection = async () => {
 
         await mongoose.connect(
             `mongodb+srv://${user}:${pwd}@${url}/${db}?retryWrites=true&w=majority&appName=Users`,
+            console.log('MongoDB connected!')
         );
         console.log('Mongo connection successfully established!');
     } catch (error) {
--- src/server.js
+++ src/server.js
@@ -12,7 +12,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(cors());
-app.use(express.json());
+//app.use(express.json());
 
 
 export async function setupServer() {
