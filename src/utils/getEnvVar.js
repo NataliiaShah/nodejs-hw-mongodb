032754b6b519e3c8cfda72e707ad2975ed0c9bfa
NataliiaShah <gavrilovskaboroluk@gmail.com>
2025-04-04 23:24:39 +0300

    correction

--- src/utils/getEnvVar.js
+++ src/utils/getEnvVar.js
@@ -3,9 +3,10 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export const getEnvVar = (name, defaultName) => {
-    const value = process.env[name];
-    if (value) return value;
-    if (defaultName) return defaultName;
-    throw new Error(`Missing: process.env['${name}']`);
+  const value = process.env[name];
 
+  if (value) return value;
+
+  if (defaultName) return defaultName;
+  throw new Error(`Missing: process.env['${name}'].`);
 };
