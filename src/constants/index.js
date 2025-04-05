c33c358eb5d14636a1c3c6e163a0a13cb4c62ab8
NataliiaShah <gavrilovskaboroluk@gmail.com>
2025-04-05 19:21:13 +0300

    correction

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,4 +1,5 @@
 import path from 'node:path';
+import { getEnvVar } from '../utils/getEnvVar.js';
 
 export const SORT_ORDER = {
   ASC: 'asc',
@@ -12,17 +13,21 @@ const ONE_DAY = 24 * 60 * 60 * 1000;
 export const THIRTY_DAYS = 30 * ONE_DAY;
 
 export const SMTP = {
-  SMTP_HOST: 'SMTP_HOST',
-  SMTP_PORT: 'SMTP_PORT',
-  SMTP_USER: 'SMTP_USER',
-  SMTP_PASSWORD: 'SMTP_PASSWORD',
-  SMTP_FROM: 'SMTP_FROM',
+  SMTP_HOST: getEnvVar('SMTP_HOST'),
+  SMTP_PORT: getEnvVar('SMTP_PORT'),
+  SMTP_USER: getEnvVar('SMTP_USER'),
+  SMTP_PASSWORD: getEnvVar('SMTP_PASSWORD'),
+  SMTP_FROM: getEnvVar('SMTP_FROM'),
 };
 export const TEMP_UPLOAD_DIR = path.join(process.cwd(), 'temp');
 export const UPLOAD_DIR = path.join(process.cwd(), 'uploads');
 
 export const CLOUDINARY = {
-  CLOUD_NAME: 'CLOUD_NAME',
-  API_KEY: 'API_KEY',
-  API_SECRET: 'API_SECRET',
+  CLOUD_NAME: getEnvVar('CLOUDINARY_CLOUD_NAME'),
+  API_KEY: getEnvVar ('CLOUDINARY_API_KEY'),
+  API_SECRET: getEnvVar('CLOUDINARY_API_SECRET'),
 };
+
+
+
+
--- src/utils/sendMail.js
+++ src/utils/sendMail.js
@@ -1,19 +1,19 @@
 import nodemailer from 'nodemailer';
-
-import { SMTP } from '../constants/index.js';
 import { getEnvVar } from '../utils/getEnvVar.js';
 
 const transporter = nodemailer.createTransport({
-  host: getEnvVar(SMTP.SMTP_HOST),
-  port: getEnvVar(SMTP.SMTP_PORT),
+  host: getEnvVar('SMTP_HOST'),
+  port: getEnvVar('SMTP_PORT'),
   auth: {
-    user: getEnvVar(SMTP.SMTP_USER),
-    pass: getEnvVar(SMTP.SMTP_PASSWORD),
+    user: getEnvVar('SMTP_USER'),
+    pass: getEnvVar('SMTP_PASSWORD'),  
   },
 });
+
 
 export const sendEmail = (options) => {
   console.log(options);
 
   return transporter.sendMail(options);
 };
+
